558d99728b4b07c575187418f765250b2fc18f1b
OrianeORDS <oriane.royon@gmail.com>
2024-06-16 17:17:26 +0200

    first commit for the ORDS web dev project !

---
+++ codewebdev/index.js
@@ -0,0 +1,17 @@
+// verify that link to js file is correct
+// document.querySelector("h1").innerHTML="Test JS OK"
+
+// Eyewink : test on the footer img 
+document.querySelector("footer img").addEventListener("mouseover",clindoeil)
+
+// Eyewink : iteration for all the images of section3 
+var s4Img = document.querySelectorAll("#section4 img")
+for (var i = 0; i < s4Img.length; i++) {
+    s4Img[i].addEventListener("mouseover",clindoeil); 
+    }
+
+function clindoeil(event){
+    event.srcElement.setAttribute("src","./images/oeil/eye2.png");
+    setTimeout(function () {
+        event.srcElement.setAttribute("src","./images/oeil/eye1.png" );}, 300);
+}
